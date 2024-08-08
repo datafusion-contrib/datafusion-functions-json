dde38ff9e17fb4c7af69ec3a26079e840d1007b5
Samuel Colvin <s@muelcolvin.com>
2024-08-08 18:53:13 +0100

    fix import

--- src/json_get_json.rs
+++ src/json_get_json.rs
@@ -1,12 +1,13 @@
 use std::any::Any;
 use std::sync::Arc;
 
-use crate::common::{check_args, get_err, invoke, jiter_json_find, GetError, JsonPath};
-use crate::common_macros::make_udf_function;
 use arrow::array::{ArrayRef, StringArray};
 use arrow_schema::DataType;
 use datafusion_common::{Result as DataFusionResult, ScalarValue};
 use datafusion_expr::{ColumnarValue, ScalarUDFImpl, Signature, Volatility};
+
+use crate::common::{check_args, get_err, invoke, jiter_json_find, GetError, JsonPath};
+use crate::common_macros::make_udf_function;
 
 make_udf_function!(
     JsonGetJson,
