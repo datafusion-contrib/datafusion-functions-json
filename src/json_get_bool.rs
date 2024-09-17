8121c5afbe6f8e8adf07cac11e36201ae0417774
Samuel Colvin <s@muelcolvin.com>
2024-09-17 17:35:00 +0100

    unpack dict for get_bool

--- src/json_get_bool.rs
+++ src/json_get_bool.rs
@@ -46,7 +46,7 @@ impl ScalarUDFImpl for JsonGetBool {
     }
 
     fn return_type(&self, arg_types: &[DataType]) -> DataFusionResult<DataType> {
-        return_type_check(arg_types, self.name(), DataType::Boolean)
+        return_type_check(arg_types, self.name(), DataType::Boolean).map(|_| DataType::Boolean)
     }
 
     fn invoke(&self, args: &[ColumnarValue]) -> DataFusionResult<ColumnarValue> {
@@ -55,7 +55,7 @@ impl ScalarUDFImpl for JsonGetBool {
             jiter_json_get_bool,
             |c| Ok(Arc::new(c) as ArrayRef),
             ScalarValue::Boolean,
-            true,
+            false,
         )
     }
 
