4446d2bb0911b4f05a743a4d48e5f5e178d05f2a
David Hewitt <david.hewitt@pydantic.dev>
2024-11-28 13:24:07 +0000

    support function unnesting for `json_as_text` inner calls (#57)

--- tests/main.rs
+++ tests/main.rs
@@ -881,6 +881,38 @@ async fn test_plan_arrow_double_nested() {
 }
 
 #[tokio::test]
+async fn test_double_arrow_double_nested() {
+    let batches = run_query("select name, json_data->>'foo'->>0 from test").await.unwrap();
+
+    let expected = [
+        "+------------------+---------------------------------------------+",
+        "| name             | test.json_data ->> Utf8(\"foo\") ->> Int64(0) |",
+        "+------------------+---------------------------------------------+",
+        "| object_foo       |                                             |",
+        "| object_foo_array | 1                                           |",
+        "| object_foo_obj   |                                             |",
+        "| object_foo_null  |                                             |",
+        "| object_bar       |                                             |",
+        "| list_foo         |                                             |",
+        "| invalid_json     |                                             |",
+        "+------------------+---------------------------------------------+",
+    ];
+    assert_batches_eq!(expected, &batches);
+}
+
+#[tokio::test]
+async fn test_plan_double_arrow_double_nested() {
+    let lines = logical_plan(r"explain select json_data->>'foo'->>0 from test").await;
+
+    let expected = [
+        "Projection: json_as_text(test.json_data, Utf8(\"foo\"), Int64(0)) AS test.json_data ->> Utf8(\"foo\") ->> Int64(0)",
+        "  TableScan: test projection=[json_data]",
+    ];
+
+    assert_eq!(lines, expected);
+}
+
+#[tokio::test]
 async fn test_arrow_double_nested_cast() {
     let batches = run_query("select name, (json_data->'foo'->0)::int from test")
         .await
@@ -908,6 +940,41 @@ async fn test_plan_arrow_double_nested_cast() {
 
     let expected = [
         "Projection: json_get_int(test.json_data, Utf8(\"foo\"), Int64(0)) AS test.json_data -> Utf8(\"foo\") -> Int64(0)",
+        "  TableScan: test projection=[json_data]",
+    ];
+
+    assert_eq!(lines, expected);
+}
+
+#[tokio::test]
+async fn test_double_arrow_double_nested_cast() {
+    let batches = run_query("select name, (json_data->>'foo'->>0)::int from test")
+        .await
+        .unwrap();
+
+    let expected = [
+        "+------------------+---------------------------------------------+",
+        "| name             | test.json_data ->> Utf8(\"foo\") ->> Int64(0) |",
+        "+------------------+---------------------------------------------+",
+        "| object_foo       |                                             |",
+        "| object_foo_array | 1                                           |",
+        "| object_foo_obj   |                                             |",
+        "| object_foo_null  |                                             |",
+        "| object_bar       |                                             |",
+        "| list_foo         |                                             |",
+        "| invalid_json     |                                             |",
+        "+------------------+---------------------------------------------+",
+    ];
+    assert_batches_eq!(expected, &batches);
+}
+
+#[tokio::test]
+async fn test_plan_double_arrow_double_nested_cast() {
+    let lines = logical_plan(r"explain select (json_data->>'foo'->>0)::int from test").await;
+
+    // NB: json_as_text(..)::int is NOT the same as `json_get_int(..)`, hence the cast is not rewritten
+    let expected = [
+        "Projection: CAST(json_as_text(test.json_data, Utf8(\"foo\"), Int64(0)) AS test.json_data ->> Utf8(\"foo\") ->> Int64(0) AS Int32)",
         "  TableScan: test projection=[json_data]",
     ];
 
